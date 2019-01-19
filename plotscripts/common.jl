b19c6411028cbbb288fd16fb1005b7100b155bd9
Zenna Tavares <zennatavares@gmail.com>
2019-01-19 00:52:17 -0500

    add common

---
+++ plotscripts/common.jl
@@ -0,0 +1,14 @@
+import Plots
+using Plots.PlotMeasures
+using LaTeXStrings
+
+colwidth = 82
+
+up = 8 #8x upscaling in resolution
+fntsm = Plots.font("sans-serif", 32)
+fntlg = Plots.font("sans-serif", 32)
+Plots.default(titlefont=fntlg, guidefont=fntlg, tickfont=fntsm, legendfont=fntsm)
+Plots.default(size=(colwidth*up,colwidth*up)) #Plot canvas size
+Plots.default(dpi=300) #Only for PyPlot - presently broken
+
+PAPERHOME = "/home/zenna/repos/papers/OmegaPaper/"
